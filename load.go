3224646839470c13d51a4c92a8e9710b0582cc97
Alex Suslov <suslov@me.com>
2019-04-19 11:50:40 +0300

    own variable get functions

---
+++ load.go
@@ -0,0 +1,69 @@
+package godotenv
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"syscall"
+)
+
+// feature
+// _test := godotenv.GetPanic("TEST_INT")
+// _test = "5" || panic("TEST_INT")
+
+// GetPanic get variable or panic
+func GetPanic(key string) string {
+	v, _ := syscall.Getenv(key)
+	if v == "" {
+		panic(key)
+	}
+	return v
+}
+
+// int value
+//_test, err := godotenv.GetIntPanic("NATS_test")
+// _test = 5 || panic("TEST_INT")
+
+// GetInt get int variable
+func GetInt(key string) (i int, err error) {
+	v, _ := syscall.Getenv(key)
+	if v == "" {
+		err = errors.New(key)
+		return
+	}
+	return strconv.Atoi(v)
+
+}
+
+// GetIntPanic get int variable or panic
+func GetIntPanic(key string) int {
+	i, err := GetInt(key)
+	if err != nil {
+		panic(key)
+	}
+	return i
+}
+
+// Gets returning a slice of substrings
+// _test := godotenv.GetsPanic("TEST_INTS")
+// _test =["1", "2", "3", "5"] || panic("TEST_INTS")
+func Gets(key string) (l []string, err error) {
+	v, _ := syscall.Getenv(key)
+	if v == "" {
+		err = errors.New(key)
+		return
+	}
+	return strings.Fields(v), nil
+}
+
+// GetsPanic returning a slice of substrings
+func GetsPanic(key string) (l []string) {
+	slice, err := Gets(key)
+	if err != nil {
+		panic(key)
+	}
+	return slice
+}
+
+// _test := godotenv.GetIntsPanic("TEST_INTS")
+//  _test = [1, 2, 3, 5] || panic("TEST_INTS")
---
+++ load_test.go
@@ -0,0 +1,51 @@
+package godotenv_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/alexsuslov/godotenv"
+)
+
+func noPanic(isPanic bool) {
+	if !isPanic {
+		return
+	}
+	if r := recover(); r != nil {
+		return
+		// fmt.Println("Recovered in f", r)
+	}
+}
+func TestGetPanic(t *testing.T) {
+	os.Setenv("TEST_INT", "5")
+	type args struct {
+		key string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  string
+		panic bool
+	}{
+		{
+			"GetPanic get variable or panic",
+			args{"TEST_INT"},
+			"5",
+			false,
+		},
+		{
+			"GetPanic get variable or panic",
+			args{"TEST_INT1"},
+			"5",
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer noPanic(tt.panic)
+			if got := godotenv.GetPanic(tt.args.key); got != tt.want {
+				t.Errorf("GetPanic() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
